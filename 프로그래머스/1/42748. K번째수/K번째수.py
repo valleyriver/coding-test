b567252a5d14a09f578bde1db180f84f5eddc4a3
홍여경 (Yeogyung Hong) <49058781+valleyriver@users.noreply.github.com>
2026-09-21 15:06:32 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,6 @@
 def solution(array, commands):
     answer = []
     for start, end, target in commands:
-        lst = sorted(array[start-1 : end])
-        answer.append(lst[target-1])
+        lst = sorted(array[start - 1:end])
+        answer.append(lst[target - 1])
     return answer
