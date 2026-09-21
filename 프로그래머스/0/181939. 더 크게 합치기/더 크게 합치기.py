979c39866984413d181e9b4db590c67d7747bd71
홍여경 (Yeogyung Hong) <49058781+valleyriver@users.noreply.github.com>
2026-09-21 16:41:02 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.00 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,5 +1,8 @@
 def solution(a, b):
     num1 = int(str(a) + str(b))
     num2 = int(str(b) + str(a))
-    answer = max(num1, num2)
+    if num1 >= num2:
+        answer = num1
+    else:
+        answer = num2
     return answer
