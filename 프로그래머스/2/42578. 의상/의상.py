4d07e9c8a863eefb94c55170cdeea481dc4d3181
홍여경 (Yeogyung Hong) <49058781+valleyriver@users.noreply.github.com>
2026-09-23 15:42:58 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+
+def solution(clothes):
+    answer = 1
+    categories = []
+    for cloth in clothes:
+        categories.append(cloth[1])
+    counter = Counter(categories)
+    counts = list(counter.values())
+    for count in counts:
+        answer *= count + 1
+    answer -= 1
+    return answer
