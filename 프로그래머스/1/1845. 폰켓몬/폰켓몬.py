016eb12f3eb96a9ee2999f1bb246cca34074419e
홍여경 (Yeogyung Hong) <49058781+valleyriver@users.noreply.github.com>
2026-09-23 11:16:54 +0900

    [level 1] Title: 폰켓몬, Time: 0.61 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,8 +1,5 @@
-from collections import Counter
-
-
 def solution(nums):
     max_count = len(nums) // 2
-    type_count = len(Counter(nums))
+    type_count = len(set(nums))
     answer = min(max_count, type_count)
     return answer
