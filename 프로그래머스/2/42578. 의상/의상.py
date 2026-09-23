e8d5b8b28e596bedc073dd98289e7e94139fd07c
홍여경 (Yeogyung Hong) <49058781+valleyriver@users.noreply.github.com>
2026-09-23 16:05:19 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -3,14 +3,16 @@ from collections import Counter
 
 def solution(clothes):
     answer = 1
-    categories = []
+    counter = {}
     
     for cloth in clothes:
-        categories.append(cloth[1])
-    counter = Counter(categories)
-    counts = list(counter.values())
-    
-    for count in counts:
+        category = cloth[1]
+        if category in counter:
+            counter[category] += 1
+        else:
+            counter[category] = 1
+            
+    for count in counter.values():
         answer *= count + 1
         
     answer -= 1
